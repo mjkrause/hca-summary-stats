476e68f8fc6ff2fc3060a2c142e0d43735f39ce3
Michael Krause <michael.j.krause@gmail.com>
2019-07-22 14:44:12 -0700

    Processes downloaded MTX file that can be loaded by scanpy

--- src/matrix_summary_stats.py
+++ src/matrix_summary_stats.py
@@ -2,14 +2,63 @@
 
 import os
 import csv
+import shutil
+import gzip
+import pandas as pd
+from zipfile import ZipFile
+from more_itertools import first
 
 
 class MatrixSummaryStats:
 
     def __init__(self, mtxdir):
-        self.genes = os.path.join(mtxdir, 'genes.tsv')
+        self.mtxdir = mtxdir
+        self.genes = None
+        self.barcodes = None
 
-    def eliminate_dupes(self):
+    def unzip_files(self, path=None) -> None:
+        with ZipFile(self.mtxdir) as archive:
+            archive.extractall(path=path)
+        matrix_path = first(os.path.splitext(self.mtxdir))  # remove extension ".zip"
+        original_dir = os.getcwd()
+        os.chdir(matrix_path)
+        files = os.listdir('.')
+        assert ['cells.tsv.gz', 'genes.tsv.gz', 'matrix.mtx.gz'] == files
+
+        for file in files:
+            with gzip.open(file, 'rb') as f_in:
+                if f_in.name == 'genes.tsv.gz' or f_in.name == 'cells.tsv.gz':
+                    self.preprocessing(f_in)  # writes files to disk
+                elif f_in.name == 'matrix.mtx.gz':
+                    outfile = first(os.path.splitext(f_in.name))
+                    with open(outfile, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+            os.remove(file)
+        files = os.listdir('.')
+        assert 'barcodes.tsv' in files
+        assert 'genes.tsv' in files
+        self.genes = os.path.abspath('genes.tsv')
+        self.barcodes = os.path.abspath('barcodes.tsv')
+        os.chdir(original_dir)
+
+    @staticmethod
+    def preprocessing(fileobj: gzip.GzipFile) -> None:
+        """Preprocessing TSV files from Matrix Service in order to use scanpy methods on matrix."""
+        f = pd.read_table(fileobj, sep='\t')  # Pandas dataframe
+        col_to_keep = []
+        if fileobj.name == 'genes.tsv.gz':
+            col_to_keep = ['featurekey', 'featurename']
+            assert col_to_keep[0] in f.columns
+            assert col_to_keep[1] in f.columns
+        elif fileobj.name == 'cells.tsv.gz':
+            fileobj.name = 'barcodes.tsv.gz'
+            col_to_keep = 'cellkey'
+            assert col_to_keep in f.columns
+        f_new = f[col_to_keep]
+        # Write to file without column or row headers.
+        f_new.to_csv(first(os.path.splitext(fileobj.name)), index=False, header=False, sep='\t')
+
+    def eliminate_dupes(self) -> list:
         # TODO: this method can probably be made more efficient
         genes = []
         with open(self.genes, 'r') as fh:
--- test/test_matrix_summary_stats.py
+++ test/test_matrix_summary_stats.py
@@ -2,15 +2,27 @@
 
 import os
 import unittest
+import shutil
 from src.matrix_summary_stats import MatrixSummaryStats
+from more_itertools import first
+
 
 class TestMatrixSummaryStats(unittest.TestCase):
 
     def setUp(self) -> None:
-        path = os.path.dirname(os.path.abspath(os.curdir))
-        self.mtxfile = os.path.join(path, 'test/data/mtxfile')
-        self.mss = MatrixSummaryStats(self.mtxfile)
+        self.path = os.path.dirname(os.path.abspath(os.curdir))
+        self.mtxdir = os.path.join(self.path,
+                                   'test/data/04b7e4ff-a4fd-4a77-9453-16cd0f7ca030.mtx.zip')
+        self.mss = MatrixSummaryStats(self.mtxdir)
 
-    def test_eliminate_dupes(self):
-        print(self.mss.eliminate_dupes())
-        self.assert
+    def tearDown(self) -> None:
+        shutil.rmtree(os.path.join(self.path, 'test/data/04b7e4ff-a4fd-4a77-9453-16cd0f7ca030.mtx'))
+
+    # def test_eliminate_dupes(self):
+    #     print(self.mss.eliminate_dupes())
+
+    def test_unzip_files(self):
+        self.mss.unzip_files(os.path.join(self.path, 'test/data'))
+        observed = os.listdir(first(os.path.splitext(self.mtxdir)))
+        expected = ['barcodes.tsv', 'matrix.mtx', 'genes.tsv']
+        self.assertEqual(observed, expected)
